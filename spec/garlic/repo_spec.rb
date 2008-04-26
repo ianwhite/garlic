ce59ab074f2ab4df56eeae8515eaa2ddbaeb1654
Ian White <ian.w.white@gmail.com>
2008-04-26 19:08:51 +0100

    Added rake tasks for specing garlic Added some more tiny specs

--- spec/garlic/repo_spec.rb
+++ spec/garlic/repo_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe Garlic::Repo do
-  describe "new(:url => 'some/url', :path => 'some/path')" do
+  describe "new(:url => 'some/local/repo', :path => 'some/dest')" do
     before { @repo = Garlic::Repo.new(:url => 'some/local/repo', :path => 'some/dest') }
     
     it "should expand the path" do
@@ -17,10 +17,22 @@ describe Garlic::Repo do
     end
     
     describe '#install' do
+      before do
+        @repo.stub!(:puts) # silence!
+      end
+      
       it "should 'git clone <repo> <dest>" do
         @repo.should_receive(:sh).with("git clone #{@repo.url} #{@repo.path}")
         @repo.install
       end
+    end
+  end
+  
+  describe "new(:url => 'git://remote/repo', :path => 'some/dest')" do
+    before { @repo = Garlic::Repo.new(:url => 'git://remote/repo', :path => 'some/dest') }
+    
+    it "should NOT expand the url" do
+      @repo.url == 'git://remote/repo'
     end
   end
 end
