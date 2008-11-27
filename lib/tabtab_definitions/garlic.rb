4dd3e6a8091440e759f011d7d94a1b58ab524c1b
Ian White <ian.w.white@gmail.com>
2008-11-27 16:24:01 +1100

    utilise Garlic::Generator in tabtab definition

--- lib/tabtab_definitions/garlic.rb
+++ lib/tabtab_definitions/garlic.rb
@@ -1,11 +1,15 @@
 require "garlic/session"
+require "garlic/generator"
 
 TabTab::Definition.register('garlic', :import => '--help') do |g|
   Garlic::Session.commands.each do |c|
     g.command c
   end
   
-  g.command :generate do 
-    `ls ~/.garlic/templates`.split.map{|t| t.sub('.rb','')}
+  g.command :generate do
+    class << self
+      include Garlic::Generator
+    end
+    available_templates
   end
 end
